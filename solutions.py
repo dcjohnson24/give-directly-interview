62d0ae04399cace85452170f1db34b3b9b1aedf8
Dylan <dcjohnson24@gmail.com>
2022-06-05 19:01:38 -0500

    Add logistic regression, plots, and more tables.

--- solutions.py
+++ solutions.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('QtAgg')
+
+import statsmodels.formula.api as smf
 
 
 if __name__ == '__main__':
@@ -7,6 +11,7 @@ if __name__ == '__main__':
     attempts = pd.read_csv('survey_attempts.csv')
     merged = pd.merge(recipients, attempts, on='recipient_id', how='left', indicator=True)
     merged._merge.value_counts(dropna=False)
+
     # It appears that all the recipients have had some survey attempt. We can drop the _merge
     # variable now
     merged.drop(columns='_merge', inplace=True)
@@ -23,7 +28,9 @@ if __name__ == '__main__':
     merged.loc[~merged.recipient_id.isin(one_success), 'stage'] = 'Start'  
     # Find Ineligible respondents
     
-    inABC = merged.county.isin(['County A', 'County B', 'County C'])
+    # Remove the text 'County' from the column
+    merged.county = merged.county.str.replace('County ', '', regex=False)
+    inABC = merged.county.isin(['A', 'B', 'C'])
     recipient_noABC = merged.recipient_id[~inABC]
 
     merged.loc[merged.recipient_id.isin(one_success) & merged.recipient_id.isin(recipient_noABC), 'stage'] = 'Ineligible'
@@ -73,13 +80,61 @@ if __name__ == '__main__':
     # By construction, the starting group has no successful surveys. The review group has at least one
     # successful survey per person. It would be good to know what the chance of someone moving from
     # the review stage to a pay stage at a later date. 
-    # TODO code here for number of recipients that transition
-    (merged.groupby('recipient_id')['stage'].nunique() > 1).sum()
 
-    # We can also look at geographical distances to determine transportation costs
-    # For both the start and review stages, there are only 
-    # 
+    (merged.groupby('recipient_id')['stage'].nunique() > 1).sum()
+    # There do not appear to be recipients that have changed their stage. Let's check for recipients
+    # that have changed from Not Active to Active accounts
+
+    (merged.groupby('recipient_id')['account_status'].nunique() > 1).sum()
+    # No recipients that have changed from Not Active to Active accounts
+
     # Also the Start group is bigger with 148 recipients compared to only 38 in the Review group.
-    # the start group success rate only needs to be 38/148 ~ 26% to match a 100% success rate in the
-    # Review group. It would be good to check this with historical data on the conversion success rate
+    # the Start group success rate only needs to be 38/148 ~ 26% to match a 100% success rate in the
+    # Review group. It would be good to check this with historical data on the conversion success rate.
+    # Data on the cost of converting the Start group vs the Review group would also be helpful
+    # for the decision.
     merged.stage.value_counts(dropna=False)      
+
+    # 3.1
+    # Graph the relationship between age and at least one successful survey
+    merged['one_success'] = 0
+    merged.loc[merged.recipient_id.isin(one_success), 'one_success'] = 1
+    # Create bins for age broken into quartiles
+    merged['age_bin'] = pd.qcut(merged.age, q=4)
+    age_success = merged.groupby('age_bin')['one_success'].sum()
+    age = merged.groupby('age_bin')['one_success'].count()
+    results = age_success.div(age, level='age_bin') * 100
+
+    results.plot(kind='bar')
+
+    plt.xticks(rotation=0)
+    plt.xlabel('Age Group')
+    plt.ylabel('% of recipients with at least\n one successful survey')
+    plt.show()
+
+    # TODO logistic regression of age and one_success
+    logit = smf.logit('one_success ~ age', data=merged).fit()
+    print(logit.summary())
+    odds_ratios = pd.DataFrame({
+            'OR': logit.params, 
+            'Lower CI': logit.conf_int()[0],
+            'Upper CI': logit.conf_int()[1]
+        })
+    
+    odds_ratios = pd.np.exp(odds_ratios)
+
+    odds_ratios['OR'][1] - 1
+    # Each additional increase of one year in age is associated with a 18 percent decrease
+    # in odds of having at least one successful survey.
+
+    # It appears that older recipients are less likely to respond to surveys. Focusing on the 
+    # older recipients would help target those with low response rates.
+
+    # 3.2
+    # We include other factors in our logistic regression to check for confounders.
+    big_logit = smf.logit('one_success ~ time_county + age + month', data=merged).fit()
+    print(big_logit.summary())
+    # when we include time spent in the county and the month of the survey, no predictor is signficant
+
+    another_logit = smf.logit('one_success ~ account_status', data=merged).fit()
+    # account status is not a significant predictor.
